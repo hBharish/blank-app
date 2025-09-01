b249cbdf537e9d0fbfe1b961db95de75795d6c67
hBharish <harilmn10@gmail.com>
2025-09-01 14:08:20 +0300

    Only gets token if present

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,12 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-a = st.query_params['request_token']
-st.write(a)
+
+params = st.experimental_get_query_params()
+
+user_id = params.get('request_token', [None])[0]
+
+if user_id:
+    st.write(f"Request Token: {user_id}")
+else:
+    st.write("User ID is not provided in the URL.")
