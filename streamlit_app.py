367a488e9620b34ebaf0d81a698e69e35f2e96ca
hBharish <harilmn10@gmail.com>
2025-09-01 14:02:19 +0300

    minor change

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-a = st.query_params['requesttoken']
+a = st.query_params['request_token']
 st.write(a)
