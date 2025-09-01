03a85ce8bd078e45f9db4e1f6f4ac14fbd1f31a2
hBharish <harilmn10@gmail.com>
2025-09-01 14:32:51 +0300

    Post to session/token and gets response

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import hashlib
+import requests
 api_key = '9bjokluovk546xbx'
 api_secret = "38iqdsm18i7oklyppfdagmkg2jqnqg5f"
 
@@ -10,12 +11,39 @@ st.write(
 
 params = st.experimental_get_query_params()
 
-user_id = params.get('request_token', [None])[0]
+request_token = params.get('request_token', [None])[0]
 
-if user_id:
+if request_token:
     st.write(f"Request Token: {user_id}")
     checksum = hashlib.sha256() 
-    checksum.update((api_key+api_secret+user_id).encode('utf-8'))
+    checksum.update((api_key+api_secret+request_token).encode('utf-8'))
     st.write(checksum.hexdigest())
 else:
     st.write("User ID is not provided in the URL.")
+
+
+
+url = "https://api.kite.trade/session/token"
+
+
+data = {
+    "api_key": api_key,
+    "request_token": request_token,
+    "checksum": checksum
+}
+
+# Set the headers
+headers = {
+    "X-Kite-Version": "3"
+}
+
+# Send the POST request
+response = requests.post(url, data=data, headers=headers)
+
+# Print the response (the access token)
+if response.status_code == 200:
+    json_data = response.json()
+    access_token = json_data['data']['access_token']
+    print(access_token)
+else:
+    print("Error:", response.text)
